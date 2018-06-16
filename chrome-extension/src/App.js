c1027bcd0515c37962f30e70a949f11cbea97ee6
syzer3@gmail.com <syzer3@gmail.com>
2018-06-16 19:30:54 +0200

    chrome-extension: add initial UI

--- chrome-extension/src/App.js
+++ chrome-extension/src/App.js
@@ -1,15 +1,64 @@
 import React, { Component } from 'react'
 import verified from './icon-verified.png'
+import unVerified from './icon-not-verified.svg'
+import iconLoading from './icon-loading.png'
 import './App.css'
 
+window.chrome.browserAction = window.chrome.browserAction  || {
+  setBadgeText: () => {
+  }
+}
+
 class App extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      icon: iconLoading,
+      verifiedString: 'Verified',
+      payed: false
+    }
+
+    // payment API/ smart contract
+    setTimeout(() => {
+      console.error(window.location)
+      this.setUnverified()
+    }, 1500)
+  }
+
+  setUnverified = () => {
+    window.chrome.browserAction.setBadgeText({ text: 'X' })
+    this.setState({
+      icon: unVerified,
+      verifiedString: 'Un Verified! Warning danger website.',
+    })
+  }
+
+  setVerified = () => {
+    window.chrome.browserAction.setBadgeText({ text: 'V' })
+    this.setState({
+      icon: verified,
+      verifiedString: 'All assets on that page are verified.',
+    })
+
+    // Bellow done't work on canary, why ?
+    // @see https://developer.chrome.com/extensions/browserAction
+    // eslint-disable-next-line
+    // chrome.browserAction.setIcon({
+    //   path : 'icon-verified.png'
+    // })
+  }
+
   render() {
+    console.log('react app running 2')
     return (
       <div className="App">
         <header className="App-header">
-          <img src={verified} className="App-logo" alt="logo" />
-          <h1 className="App-title">Your content is:</h1>
-          <img src="https://ipfs.io/ipfs/QmVZdABgbYJFwDnPxficeH6h7RpqnKvzCnMqz3TZnwaE1L" alt="adapter stack" height={200}/>
+          <img src={this.state.icon} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Your content is: {this.state.verifiedString}</h1>
+          <img
+            src="https://ipfs.io/ipfs/QmVZdABgbYJFwDnPxficeH6h7RpqnKvzCnMqz3TZnwaE1L" alt="adapter stack"
+            height={200}/>
         </header>
       </div>
     )
