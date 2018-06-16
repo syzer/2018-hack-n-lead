7b609975eb255d3398667ec916ec8663392d2b89
syzer3@gmail.com <syzer3@gmail.com>
2018-06-16 16:03:09 +0200

    docs: chrome extension : how to build

--- chrome-extension/src/App.js
+++ chrome-extension/src/App.js
@@ -9,13 +9,11 @@ class App extends Component {
         <header className="App-header">
           <img src={verified} className="App-logo" alt="logo" />
           <h1 className="App-title">Your content is:</h1>
+          <img src="https://ipfs.io/ipfs/QmVZdABgbYJFwDnPxficeH6h7RpqnKvzCnMqz3TZnwaE1L" alt="adapter stack" height={200}/>
         </header>
-        {/*<p className="App-intro">*/}
-          {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
-        {/*</p>*/}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- readme.md
+++ readme.md
@@ -13,4 +13,16 @@ TODO please put your work description here
 These we use bec asue the w3c.org didn't accepted our proposal yet, maybe because thhey are on weekend/holiday....
 Who knows? 
 
+- visit
+[](chrome://extensions/)
+
+```bash
+cd chrome-extensions
+yarn
+yarn build
+drag `build` over to chrome extensions
+# you might use reload button in chrome to refresh your extension 
+```
+
+
 - ....
