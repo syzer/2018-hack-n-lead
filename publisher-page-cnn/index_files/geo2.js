0d04ef45e4ba70af67a8d1be1dff628f06545144
syzer3@gmail.com <syzer3@gmail.com>
2018-06-17 14:07:45 +0200

    Add Cnn page made by Nigerian Prince

---
+++ publisher-page-cnn/index_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "MAUR",
+      'continent': "EU",
+      'country': "CH",
+      'region': "ZH"
+    },
+    'ip':"213.160.32.164"
+  }]);
+})
+//
+()
+
+;
