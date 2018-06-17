e787c53c8a63057074c2028884ea888441f624d6
syzer3@gmail.com <syzer3@gmail.com>
2018-06-17 11:33:03 +0200

    WIP: smartCOntract

--- chrome-extension/src/App.js
+++ chrome-extension/src/App.js
@@ -4,24 +4,25 @@ import unVerified from './icon-not-verified.svg'
 import iconLoading from './icon-loading.png'
 import './App.css'
 
-window.chrome.browserAction = window.chrome.browserAction  || {
+window.chrome.browserAction = window.chrome.browserAction || {
   setBadgeText: () => {
   }
 }
 
 class App extends Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       icon: iconLoading,
-      verifiedString: 'Verified',
-      payed: false
+      payed: false,
+      resources: [],
+      verifiedString: 'Verified'
     }
 
     // payment API/ smart contract
     setTimeout(() => {
-      console.error(window.location)
+      // console.error(JSON.stringify(window.location))
       this.setUnverified()
     }, 1500)
   }
@@ -30,9 +31,50 @@ class App extends Component {
     window.chrome.browserAction.setBadgeText({ text: 'X' })
     this.setState({
       icon: unVerified,
-      verifiedString: 'Un Verified! Warning danger website.',
+      verifiedString: 'Unverified! Warning dangerous website.',
+    })
+
+    let data = 'UnVerified'
+
+    // eslint-disable-next-line
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+      // eslint-disable-next-line
+      chrome.tabs.sendMessage(tabs[0].id, {data}, response => {
+        console.log('response', response)
+        // eslint-disable-next-line
+        this.setState({
+          resources: [response]
+        })
+        console.log('success')
+      })
     })
   }
+
+
+  // componentDidMount() {
+  //   // Add listener when component mounts
+  //   window.chrome.runtime.onMessage.addListener(this.handleMessage)
+  // }
+  //
+  // handleMessage = (msg) => {
+  //   alert(msg)
+  //   // Handle received messages
+  //   if (msg.target === 'app') {
+  //     if (msg.type === 'setMessage') {
+  //       this.setState({ message: msg.body })
+  //     }
+  //   }
+  // }
+  //
+  // componentWillUnmount() {
+  //   alert('ssss')
+  //   // Remove listener when this component unmounts
+  //   window.chrome.runtime.onMessage.removeListener(this.handleMessage)
+  // }
+
+  // componentWillUnmount = () => {
+  //   window.chrome.browserAction.setBadgeText({ text: '' })
+  // }
 
   setVerified = () => {
     window.chrome.browserAction.setBadgeText({ text: 'V' })
@@ -56,6 +98,8 @@ class App extends Component {
         <header className="App-header">
           <img src={this.state.icon} className="App-logo" alt="logo"/>
           <h1 className="App-title">Your content is: {this.state.verifiedString}</h1>
+          {this.state.resources.map((e, i) =>
+            <p key={i}>Resource: {e}</p>)}
           <img
             src="https://ipfs.io/ipfs/QmVZdABgbYJFwDnPxficeH6h7RpqnKvzCnMqz3TZnwaE1L" alt="adapter stack"
             height={200}/>
