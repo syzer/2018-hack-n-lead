3d411500bb90cac90234f4e3a6d7d4d7ae869cc7
Tobi Turing <hexagon6@fet.li>
2018-06-17 14:57:56 +0200

    wip

--- chrome-extension/public/main.js
+++ chrome-extension/public/main.js
@@ -1,14 +1,62 @@
+// if (typeof web3 !== 'undefined') {
+//   window.web3 = new Web3(web3.currentProvider)
+// } else {
+
+// }
+
+// const config = {
+//   contractAddress: '0x651493d194e7cdb2561e9afd41ea2db9a55e3f0c',
+//   contractABI: require('./abi.json')
+// }
+
 const images = document.querySelectorAll("img")
 let verified = true
 
-// TODO here solidity
-const verifyHash = hash => hash == '34232432dasdsf'
+console.log('foo:' + md5('foo'))
+
+// const getHashFromBlockchain = function (address) {
+//   // solidity something something
+//   const contract = window.web3.eth.contract(config.contractABI).at(contractAddress)
+
+//   // 1. get all external components with attribute ref and extract ref & src attribute
+//   // 2. do the checksum of the src content
+//   // 3. compare the generated checksum with the contract content checksum from the blockchain
+//   // 4. success or fail
+//   contract
+//   // return hash
+//   return '34232432dasdsf'
+// }
+
+// const scrapeRefs = ''
+
+// // TODO here solidity
+// const verifyHash = address => {
+//   return hash == getHashFromBlockchain(address)
+// }
+
+// function getBase64(file) {
+//    var reader = new FileReader();
+//    reader.readAsDataURL(file);
+//    reader.onload = function () {
+//      hash
+//      console.log(reader.result);
+//    };
+//    reader.onerror = function (error) {
+//      console.log('Error: ', error);
+//    };
+// }
+
+
+
+// var file = document.querySelector('#files > input[type="file"]').files[0];
+// getBase64(file); // prints the base64 string
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   let data = request.data || {}
   console.log('something happening from the extension', data)
 
   images.forEach(img => {
+    // hook in here sha
     if (!img.attributes.ver) {
       verified = false
       img.classList.add('unverified')
