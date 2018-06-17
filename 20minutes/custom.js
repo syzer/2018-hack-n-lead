65b9c58304d1cb8679f752fd6b2dd9c3fca02aad
syzer3@gmail.com <syzer3@gmail.com>
2018-06-17 16:30:46 +0200

    To nataliya

--- 20minutes/custom.js
+++ 20minutes/custom.js
@@ -1,7 +1,7 @@
 $(window).on('load', function () {
   // on Ropsten
   var contractAddress = "0x3fd2f126495a5e02ddaea1b1a36027b8c1c0ed58";
-  var buyersAddress = "0xbc3c30edd3b1894a1f29af1edcd4a3308f02b6ef";
+  var buyerAddress = "0xbc3c30edd3b1894a1f29af1edcd4a3308f02b6ef";
   var contractAbi = [
     {
       "constant": false,
@@ -81,6 +81,8 @@ $(window).on('load', function () {
     }
   ];
 
+  $('#verify').on('click', verify)
+
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
     // Use Mist/MetaMask's provider
@@ -98,10 +100,11 @@ $(window).on('load', function () {
 
   function verify() {
     // TODO put buyers addres
-    let savedHash = contractInstance.content({ from: buyerAddress });
-
+    let savedHash = contractInstance.content({ from: buyerAddress, gas: 470400 }, console.log)
     let computedHash = '4ee335e88f548039eea9b1e7de3aa282'
-    return (savedHash == computedHash && contractInstance.check_legal({ from: buyerAddress }));
+    // return (savedHash == computedHash && contractInstance.check_legal({ from: buyerAddress }));
   }
+
+  console.log('VERIFIED ?')
 
 })
